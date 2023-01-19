a86d5feaa7e312a0eeaf80a9f842dd009b4e416b
EC2 Default User <ec2-user@ip-172-31-2-245.ap-northeast-1.compute.internal>
2023-01-19 08:24:54 +0000

    注文、注文詳細一覧機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_053734) do
+ActiveRecord::Schema.define(version: 2023_01_19_044307) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,26 @@ ActiveRecord::Schema.define(version: 2023_01_18_053734) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "tax_included_price"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "payment_method"
+    t.integer "postage"
+    t.integer "billing_amount"
+    t.integer "status"
+    t.string "address_name"
+    t.string "postal_code"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
