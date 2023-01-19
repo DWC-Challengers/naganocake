6f79fe583fadcd74af2d101a7f97cd9323c902fa
YamaShuhei <119114032+YamaShuhei@users.noreply.github.com>
2023-01-19 04:18:40 +0000

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,10 @@ ActiveRecord::Schema.define(version: 2023_01_18_053734) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
-  
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 
   create_table "shipping_addresses", force: :cascade do |t|
     t.string "address_name"
