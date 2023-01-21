317973e9860574647dfb60675a67bb886861bb6e
EC2 Default User <ec2-user@ip-172-31-7-210.ap-northeast-1.compute.internal>
2023-01-21 08:55:36 +0000

    change minor

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,16 +7,27 @@ class Admin::OrdersController < ApplicationController
 
   #注文詳細表示用
   def show
-    @order = Order.find(params[:id])
-    @order_detail = Order_item.find(params[:id])
+    # @order = Order.find(params[:id])
+    # @order_detail = Order_item.find(params[:id])
+    # @customer = Customer.find(params[:customer_id])
+    # @name = @customer.last_name + "　" + @customer.first_name
+    
   end
 
   def update
     @order = Order.find(params[:id])
-    Order.update(order_params)
-    @order_detail = Order_item.find(params[:id])
-    Order_detail.update(order_detail_params)
-    render :show
+    @customer = Customer.find(params[:customer_id])
+    if @order.update(order_params)
+      render :show, success:"注文ステータスを更新しました"
+    else
+      render :show
+    end
+    @order_item = Order_item.find(params[:id])
+    if @order_item.update(order_detail_params)
+      render :show, detail_success:"制作ステータスを更新しました"
+    else
+      render :show
+    end
   end
   
   private
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,5 @@ belongs_to :order
 #belongs_to :item
     
     
+enum status:{ not_possible: 0,pending: 1,in_production: 2,complete: 3}
 end
