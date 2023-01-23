ec2a76aa5ca7a3779c70cf87ece6a9b7dce6bd03
EC2 Default User <ec2-user@ip-172-31-7-210.ap-northeast-1.compute.internal>
2023-01-23 15:29:25 +0000

    ヘッダー/フッターの修正、検索機能の追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,8 +1,8 @@
-class SearchesController < ApplicationController
+class Public::SearchesController < ApplicationController
 before_action :authenticate_customer!
     
   def search
-      @items = Item.looks(params[:word])
-      render "/searches/search_result"
+      @items = Item.looks(params[:word]).page(params[:page])
+      render "public/searches/search_result"
   end
 end
