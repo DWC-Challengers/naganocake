d57bdc338f17d784fc6f51d72392146573d95ee2
TBan34 <tb3358@gmail.com>
2023-01-23 03:24:12 +0000

    [Update]Public 商品一覧/詳細ページのレイアウト調整ほか

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,7 @@
 class Public::CartItemsController < ApplicationController
     
   def index
-    @cart_items=current_user.cart_items
+    @cart_items=CartItem.all
     @total=0
   end
   
@@ -10,35 +10,35 @@ class Public::CartItemsController < ApplicationController
       @cart_item=CartItem.find(params[:id])
       @cart_item.order_item_quantity += params[:cart_item][:order_item_quantity].to_i
       @cart_item.save
-      redirect_to current_cart
+      redirect_to cart_items_path
     else
       @cart_item=CartItem.new(cart_item_params)
       @cart_item.save
-      redirect_to current_cart
+      redirect_to cart_items_path
     end
   end
       
   def update
     @cart_item=CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to current_cart
+    redirect_to cart_items_path
   end
   
   def destroy
     @cart_item=CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to current_cart
+    redirect_to cart_items_path
   end
   
   def destroy_all
     current_user.cart_items.destroy_all
-    redirect_to current_cart
+    redirect_to cart_items_path
   end
   
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :order_item_quantity)
+    params.require(:cart_item).permit(:item_id, :customer_id, :order_item_quantity)
   end
 
 end
